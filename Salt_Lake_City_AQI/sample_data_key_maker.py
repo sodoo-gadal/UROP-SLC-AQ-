08ee23d04ea735ce8f15e52f7329a3558082d758
Rei Zhang <rei.zhang@berkeley.edu>
2026-09-16 20:17:56 -0700

    RZ edits - fix aqs download

--- Salt_Lake_City_AQI/sample_data_key_maker.py
+++ Salt_Lake_City_AQI/sample_data_key_maker.py
@@ -4,6 +4,7 @@
 import os
 from dotenv import load_dotenv
 import pandas as pd
+import requests
 
 
 load_dotenv()
@@ -14,29 +15,49 @@ load_dotenv()
 #end date is 20260731 YYYYMMDD
 
 
-def make_sample_data_key(begin_date, end_date):
-    state_code = 49 #utah's code
+def make_sample_data_key(site_number, parameter_code, begin_date, end_date):
+    state_code = "49" #utah's code
     county_code = "035" #salt lake county's code, having trouble including it as 035
     email = os.getenv("EMAIL")
     api_key = os.getenv("KEY")
-    data_key = f"https://aqs.epa.gov/data/api/sampleData/bySite?email={email}&key={api_key}&param={parameter_code}&bdate={begin_date}&edate={end_date}&state={state_code}&county={county_code}&site={site_number}"
-    return data_key
+    site_number = str(site_number).zfill(4)   # this ensures 4-digit site string
+    parameter_code = str(parameter_code)   # this ensures parameter code is a string
+    url = "https://aqs.epa.gov/data/api/sampleData/bySite"
+    params = {
+        "email": email,
+        "key": api_key,
+        "param": parameter_code,
+        "bdate": begin_date,
+        "edate": end_date,
+        "state": state_code,
+        "county": county_code,
+        "site": site_number
+    }
+    
+    response = requests.get(url, params=params)
+    return response
 
-site_number = input("Please input the site number (4 digits) for the site you want to get data for: ")
-parameter_code = input("Please input the parameter code for the data you want to get: ")
+site_number = "3006"
+parameter_code = "88101"
 
-key_list = []
+all_data = []
 
-for i in range(28):
-    if i == 0:
-        new_key = make_sample_data_key(19980101, 19981231)
-    elif i == 28:
-        new_key = make_sample_data_key(20260101, 20260731)
+for i in range(29): # changed to 29 to include the last date range
+    year = 1998 + i
+    print(f"Fetching data for year: {year}")
+    if year == 2026:
+        response = make_sample_data_key(site_number, parameter_code, "20260101", "20260731")
     else:
-        new_start_date = 19980101 + (i * 10000)
-        new_end_date = 19980101 + ((i + 1) * 10000) - 1
-        new_key = make_sample_data_key(new_start_date, new_end_date)
-    key_list.append(new_key)
+        new_start_date = f"{year}0101"
+        new_end_date = f"{year}1231"
+        response = make_sample_data_key(site_number, parameter_code, new_start_date, new_end_date)
 
-df = pd.DataFrame(key_list, columns=["Data Key"])
+    if response.status_code == 200:
+        json_resp = response.json()
+        if json_resp.get("Data"):
+            all_data.extend(json_resp["Data"])
+        else:
+            print(f"No data / error for {year}: {json_resp.get('Header')}")
+
+df = pd.DataFrame(all_data)
 print(df)
